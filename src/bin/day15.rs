5ebeef5541d21ac5c060ce360ea89906a255c8c2
dzil123 <5725958+dzil123@users.noreply.github.com>
2022-12-16 03:28:57 -0800

    day 15 part 2

--- src/bin/day15.rs
+++ src/bin/day15.rs
@@ -34,12 +34,53 @@ macro_rules! dbg {
     };
 }
 
+fn part2(data: &[((i32, i32), (i32, i32))]) -> (i32, i32) {
+    // let max = 20;
+    let max = 4_000_000;
+
+    let data: Vec<_> = data
+        .iter()
+        .map(|&(sensor, beacon)| (sensor, distance(sensor, beacon) + 1))
+        .collect();
+
+    let valid_guess = |guess: (i32, i32)| -> bool {
+        if guess.0 < 0 || guess.0 > max || guess.1 < 0 || guess.1 > max {
+            return false;
+        }
+        for &(sensor, dist) in &data {
+            if distance(sensor, guess) < dist {
+                return false;
+            }
+        }
+        true
+    };
+
+    for &(sensor, dist) in &data {
+        for i in 0..dist {
+            let x = (sensor.0 - i, sensor.0 + i);
+            let y = (sensor.1 - dist + i, sensor.1 + dist - i);
+            let tl = (x.0, y.0);
+            let tr = (x.1, y.0);
+            let bl = (x.0, y.1);
+            let br = (x.1, y.1);
+
+            for guess in [tl, tr, bl, br] {
+                if valid_guess(guess) {
+                    return guess;
+                }
+            }
+        }
+    }
+
+    panic!("not found")
+}
+
 fn main() {
     let data = parse();
     // dbg!(&data);
 
-    // let row = 10;
-    let row = 2000000;
+    let row = 10;
+    // let row = 2000000;
     let mut no_beacon: HashSet<i32> = HashSet::new();
 
     for &(sensor, beacon) in &data {
@@ -47,13 +88,13 @@ fn main() {
         let projected = (sensor.0, row);
         let proj_dist = distance(sensor, projected);
         let d = dist - proj_dist;
-        dbg!(((sensor, beacon), proj_dist, dist, d));
+        // dbg!(((sensor, beacon), proj_dist, dist, d));
         if d < 0 {
             continue;
         }
         let x = sensor.0;
         let rng = (x - d)..=(x + d);
-        dbg!(&rng);
+        // dbg!(&rng);
         for x in rng {
             no_beacon.insert(x);
         }
@@ -66,4 +107,11 @@ fn main() {
     }
 
     dbg!(no_beacon.len());
+
+    {
+        let res = part2(&data);
+        let res = (res.0 as u64, res.1 as u64);
+
+        dbg!(res.0 * 4000000 + res.1);
+    }
 }
