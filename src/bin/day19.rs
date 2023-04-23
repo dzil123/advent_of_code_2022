9ec54760ec378955adf1bbe65ceff7fe773b853b
dzil123 <5725958+dzil123@users.noreply.github.com>
2023-04-23 15:15:28 -0700

    day 19 wip

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -2,14 +2,14 @@ use std::collections::VecDeque;
 
 #[derive(Clone, Debug)]
 struct Cost {
-    ore: u64,
-    clay: u64,
-    obsidian: u64,
+    ore: u32,
+    clay: u32,
+    obsidian: u32,
 }
 
 #[derive(Clone, Debug)]
 struct Blueprint {
-    idx: u64,
+    idx: u32,
     robot_ore: Cost,
     robot_clay: Cost,
     robot_obsidian: Cost,
@@ -18,20 +18,21 @@ struct Blueprint {
 
 #[derive(Clone, Debug, Default)]
 struct SimResource {
-    robots: u64,
-    inventory: u64,
+    robots: u32,
+    inventory: u32,
 }
 
 #[derive(Clone, Debug, Default)]
-struct Timestep {
-    time_left: u64,
+struct Timestep<'a> {
+    time_left: u32,
     ore: SimResource,
     clay: SimResource,
     obsidian: SimResource,
     geode: SimResource,
+    last: Option<&'a Timestep<'a>>,
 }
 
-impl Timestep {
+impl<'a> Timestep<'a> {
     fn try_buy(
         &self,
         cost: &Cost,
@@ -47,51 +48,105 @@ impl Timestep {
         Some(this)
     }
 
-    fn tick(&self) -> Option<Self> {
+    fn tick(&'a self) -> Option<Self> {
         let mut this = self.clone();
         this.time_left = this.time_left.checked_sub(1)?;
         this.ore.inventory += this.ore.robots;
         this.clay.inventory += this.clay.robots;
         this.obsidian.inventory += this.obsidian.robots;
         this.geode.inventory += this.geode.robots;
+        this.last = Some(self);
 
         Some(this)
     }
 
-    fn score(&self, bp: &Blueprint) -> u64 {
+    fn score(&self, bp: &Blueprint) -> u32 {
         bp.idx * self.geode.inventory
+    }
+
+    fn print(&self) -> u8 {
+        let minute = if let Some(state) = self.last {
+            state.print()
+        } else {
+            1
+        };
+
+        println!("== Minute {minute} ==");
+        if self.ore.robots > 0 {
+            println!(
+                "{0} ore-collecting robots collect {0} ore; you now have {1} ore.",
+                self.ore.robots,
+                self.ore.inventory + self.ore.robots
+            );
+        }
+        if self.clay.robots > 0 {
+            println!(
+                "{0} clay-collecting robots collect {0} clay; you now have {1} clay.",
+                self.clay.robots,
+                self.clay.inventory + self.clay.robots
+            );
+        }
+        if self.obsidian.robots > 0 {
+            println!(
+                "{0} obsidian-collecting robots collect {0} obsidian; you now have {1} obsidian.",
+                self.obsidian.robots,
+                self.obsidian.inventory + self.obsidian.robots
+            );
+        }
+        if self.geode.inventory > 0 {
+            println!(
+                "{0} geode-collecting robots collect {0} geode; you now have {1} geode.",
+                self.geode.robots,
+                self.geode.inventory + self.geode.robots
+            );
+        }
+        println!();
+
+        if minute == 22 {
+            panic!();
+        }
+
+        minute + 1
     }
 }
 
-fn simulate_once(state: &Timestep, bp: &Blueprint) -> u64 {
-    let mut best_score = 0u64;
+fn simulate_once(state: &Timestep, bp: &Blueprint) -> u32 {
+    let mut best_score = 0u32;
     // let Some(state) = state.tick() else {
     //     best_score = best_score.max(state.score(bp));
     //     return best_score;
     // };
 
-    if let Some(state) = state.try_buy(&bp.robot_ore, &|s| &mut s.ore) {
+    if let Some(state) = state.try_buy(&bp.robot_geode, &|s| &mut s.geode) {
         // best_score = best_score.max(simulate_once(&state, bp));
         if let Some(state) = state.tick() {
             best_score = best_score.max(simulate_once(&state, bp));
-        }
-    }
-    if let Some(state) = state.try_buy(&bp.robot_clay, &|s| &mut s.clay) {
-        // best_score = best_score.max(simulate_once(&state, bp));
-        if let Some(state) = state.tick() {
-            best_score = best_score.max(simulate_once(&state, bp));
+        } else {
+            state.print();
         }
     }
     if let Some(state) = state.try_buy(&bp.robot_obsidian, &|s| &mut s.obsidian) {
         // best_score = best_score.max(simulate_once(&state, bp));
         if let Some(state) = state.tick() {
             best_score = best_score.max(simulate_once(&state, bp));
+        } else {
+            state.print();
         }
     }
-    if let Some(state) = state.try_buy(&bp.robot_geode, &|s| &mut s.geode) {
+    if let Some(state) = state.try_buy(&bp.robot_clay, &|s| &mut s.clay) {
         // best_score = best_score.max(simulate_once(&state, bp));
         if let Some(state) = state.tick() {
             best_score = best_score.max(simulate_once(&state, bp));
+        } else {
+            state.print();
+        }
+    }
+    if let Some(state) = state.try_buy(&bp.robot_ore, &|s| &mut s.ore) {
+        // best_score = best_score.max(simulate_once(&state, bp));
+        if let Some(state) = state.tick() {
+            best_score = best_score.max(simulate_once(&state, bp));
+        } else {
+            state.print();
         }
     }
 
@@ -99,12 +154,13 @@ fn simulate_once(state: &Timestep, bp: &Blueprint) -> u64 {
         best_score = best_score.max(simulate_once(&state, bp));
     } else {
         best_score = best_score.max(state.score(bp));
+        state.print();
     }
 
     best_score
 }
 
-fn simulate(bp: &Blueprint) {
+fn simulate(bp: &Blueprint) -> u32 {
     let init_state = Timestep {
         time_left: 21,
         ore: SimResource {
@@ -119,10 +175,10 @@ fn simulate(bp: &Blueprint) {
     // let mut queue = VecDeque::new();
     // queue.push_back(init_state);
 
-    // let mut best_score = 0u64;
+    // let mut best_score = 0u32;
     // let mut end_states = 0u64;
 
-    // let mut i = 0u64;
+    // let mut i = 0u32;
 
     // while let Some(state) = queue.pop_front() {
     //     // dbg!(state.time_left);
@@ -148,7 +204,7 @@ fn simulate(bp: &Blueprint) {
     //     queue.push_front(state);
 
     //     i += 1;
-    //     if i >= 1000000000 {
+    //     if i >= 1000000000u32 {
     //         dbg!(queue.len());
     //         dbg!(&queue);
     //         dbg!(end_states);
@@ -158,6 +214,7 @@ fn simulate(bp: &Blueprint) {
 
     // dbg!(&end_states);
     dbg!(best_score);
+    best_score
 }
 
 fn parse() -> Vec<Blueprint> {
@@ -201,6 +258,25 @@ fn parse() -> Vec<Blueprint> {
 
 fn main() {
     let bps = parse();
+    let bps = &bps[..1];
 
-    simulate(&bps[0]);
+    // simulate(&bps[0]);
+
+    let results: Vec<_> = std::thread::scope(|scope| {
+        let threads: Vec<_> = bps
+            .iter()
+            .map(|bp| (bp.idx, scope.spawn(|| simulate(bp))))
+            .collect();
+
+        threads
+            .into_iter()
+            .map(|(bp, thread)| (bp, thread.join().unwrap()))
+            .collect()
+    });
+
+    dbg!(&results);
+
+    dbg!(results.iter().max_by_key(|(_, score)| score));
+
+    dbg!(results.iter().map(|(id, score)| id * score).sum::<u32>());
 }
